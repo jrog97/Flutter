3286cdf31b796ceb6606521cf48f0988abf49708
berunda <jonberunda@gmail.com>
2020-04-09 20:54:24 -0400

    TEST

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterShare',
       debugShowCheckedModeBanner: false,
-      home: Text("Edit"),
+      home: Text("TEST"),
     );
   }
 }
